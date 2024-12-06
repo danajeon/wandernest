a2d1d568f3f669cd0a6542a3340b82e74c7fc99d
Dana Jeon <118026410+danajeon@users.noreply.github.com>
2024-12-05 19:47:23 -0500

    react + vite + tailwind

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './index.css' // Tailwind CSS import
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1 className="text-4xl font-bold">Tailwind CSS is working!</h1>
+    </>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/* @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './index.html',  // Ensure it includes the HTML template
+    './src/**/*.{js,ts,jsx,tsx}', // Include React components
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
