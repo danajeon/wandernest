5ec48094f1412fbc2fa618698ab8e921b80c20d7
Jun Kyung Lee <junlee0325@gmail.com>
2025-02-18 18:54:37 -0500

    Altered styling on CreateAccount and Button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const Button = ({ variant = 'default', text }) => {
     // default button
-    const baseStyle = 'px-20 py-2 rounded-2xl text-lg bg-gradient-to-r from-wandernest-blue to-wandernest-pink text-white';
+    const baseStyle = 'w-[200px] py-2 rounded-2xl text-lg bg-gradient-to-r from-wandernest-blue to-wandernest-pink text-white';
     // button variants
     const variants = {
         "inverse": 'text-transparent bg-clip-text'
--- src/components/CreateAccount.jsx
+++ src/components/CreateAccount.jsx
@@ -3,11 +3,11 @@ import { Button } from "./Button"
 
 export const CreateAccount = ({handleSignIn}) => {
   return (
-    <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 space-y-8 border border-solid border-black bg-white rounded-3xl flex flex-col items-center px-[20%] py-[3%] h-[40%]">
-      <div className="-mb-6">
-        <span className="text-transparent bg-clip-text bg-gradient-to-r from-wandernest-blue to-wandernest-pink text-6xl">Create Account</span>
+    <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 space-y-8 border border-solid border-black bg-white rounded-3xl flex flex-col items-center w-[50%] py-[2%]">
+      <div>
+        <span className="text-transparent bg-clip-text bg-gradient-to-r from-wandernest-blue to-wandernest-pink text-5xl">Create Account</span>
       </div>
-      <div className="flex flex-col items-center text-2xl text-gray-400">
+      <div className="flex flex-col items-center text-xl text-gray-400">
         <div className="flex flex-col">
           <label htmlFor="username">Username</label>
           <input type="email" className="border border-gray-400 text-black rounded-lg focus:outline-none focus:ring-2 px-1"/>
