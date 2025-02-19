07d7b6ed5aed961943234553dcddea34035180e0
Jun Kyung Lee <junlee0325@gmail.com>
2025-02-19 04:53:04 -0500

    Recreated intro animation per Figma

--- src/components/CreateAccount.jsx
+++ src/components/CreateAccount.jsx
@@ -3,9 +3,9 @@ import { Button } from "./Button"
 
 export const CreateAccount = ({handleSignIn}) => {
   return (
-    <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 space-y-8 border border-solid border-black bg-white rounded-3xl flex flex-col items-center w-[50%] py-[2%]">
-      <div>
-        <span className="text-transparent bg-clip-text bg-gradient-to-r from-wandernest-blue to-wandernest-pink text-5xl">Create Account</span>
+    <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/4 space-y-8 bg-white rounded-3xl flex flex-col items-center w-[50%] py-[2%]">
+      <div className="flex justify-center text-center">
+        <span className="text-transparent bg-clip-text bg-gradient-to-r from-wandernest-blue to-wandernest-pink text-5xl py-1">Create Account</span>
       </div>
       <div className="flex flex-col items-center text-xl text-gray-400">
         <div className="flex flex-col">
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,30 @@
 /* @type {import('tailwindcss').Config} */
 export default {
   content: [
-    './index.html',  // Ensure it includes the HTML template
-    './src/**/*.{js,ts,jsx,tsx}', // Include React components
+    "./index.html", // Ensure it includes the HTML template
+    "./src/**/*.{js,ts,jsx,tsx}", // Include React components
   ],
   theme: {
     extend: {
       fontFamily: {
-        'itim': ['Itim', 'serif']
+        itim: ["Itim", "serif"],
       },
       colors: {
-        'wandernest-blue': '#B0E7FF',
-        'wandernest-pink': '#E589D9',
-        'wandernest-purple': '#BFCCFB',
+        "wandernest-blue": "#B0E7FF",
+        "wandernest-pink": "#E589D9",
+        "wandernest-purple": "#BFCCFB",
+      },
+      animation: {
+        fadeIn: "fadeIn .9s ease-in forwards",
+        fadeOut: "fadeOut .9s ease-in forwards",
+        slideUp: "slideUp .9s ease-in forwards"
+      },
+      keyframes: {
+        fadeIn: { "0%": { opacity: "0" },"70%": {opacity: "0"}, "100%": { opacity: "1" } },
+        fadeOut: { "0%": { opacity: "1",top: "50%" },"70%": {opacity: "1",top: "50%"}, "80%": { opacity: "0",top: "33%" },"100%": {opacity: "0",top: "33%"} },
+        slideUp: { "0%": { top: "50%" }, "100%": { top: "33%" } },
       },
     },
+    plugins: [],
   },
-  plugins: [],
-}
-
+};
