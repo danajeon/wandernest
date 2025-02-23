7e85cb939387f044c050c56419d6e32d91e6544c
Jun Kyung Lee <junlee0325@gmail.com>
2025-02-23 18:09:36 -0500

    update Button style

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const Button = ({ variant = 'default', onClick, text }) => {
     // default button
-    const baseStyle = 'w-[200px] py-2 rounded-2xl text-lg bg-gradient-to-r from-wandernest-blue to-wandernest-pink text-white transition-all duration-150 hover:border-2';
+    const baseStyle = 'w-[200px] py-1 rounded-lg text-lg bg-gradient-to-r from-wandernest-blue to-wandernest-pink text-white border   hover:text-transparent hover:bg-clip-text';
     // button variants
     const variants = {
         "inverse": 'text-transparent bg-clip-text'
