754bba1ddb5aaedb5ecd1f07a31a7ff70a9c2357
Jun Kyung Lee <junlee0325@gmail.com>
2025-02-23 16:37:22 -0500

    Co-authored-by: Dana <danajeon@users.noreply.github.com>

--- src/components/CreateTrip.jsx
+++ src/components/CreateTrip.jsx
@@ -1,26 +1,44 @@
 import React from 'react'
+import { useState, useEffect } from 'react';
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import MenuIcon from '@mui/icons-material/Menu';
 import { Button } from './Button'
 
 export const CreateTrip = () => {
+    const [tripName, setTripName] = useState("")
+    const [startDate, setStartDate] = useState()
+    const [endDate, setEndDate] = useState()
+
+    useEffect(() => {
+        console.log("v Trip Detail v")
+        console.log(tripName)
+        console.log(startDate)
+        console.log(endDate)
+    },[tripName,startDate,endDate])
+
   return (
     <div className='h-full'>
         <div className='flex flex-row justify-between items-center px-4 h-[8%]'>
-            <ArrowBackIcon fontSize='large'/>
-            <h2 className='text-2xl'>New trip</h2>
-            <MenuIcon fontSize='large'/>
+            <ArrowBackIcon fontSize='large' sx={{
+            'cursor': 'pointer',
+          }}/>
+            <h2 className='text-2xl'>New Trip</h2>
+            <MenuIcon fontSize='large' sx={{
+            'cursor': 'pointer',
+          }}/>
         </div>
         <div className='flex flex-col justify-between items-center h-[80%] rounded-md m-6 p-4 bg-gray-300'>
             <div className='flex flex-col w-full'>
+
                 <label htmlFor="tripName">Trip Name</label>
-                <input type="text" className='rounded-md focus:outline-none focus:ring-2 mb-4 p-1' />
+                <input type="text" className='rounded-md focus:outline-none focus:ring-2 mb-4 p-1' onChange={(e) => setTripName(e.target.value)} />
+
                 <label htmlFor="StartDate">Start Date</label>
-                <input type="text" className='rounded-md focus:outline-none focus:ring-2 mb-4 p-1' />
+                <input type="date" className='rounded-md focus:outline-none focus:ring-2 mb-4 p-1' onChange={(e) => setStartDate(e.target.value)}/>
+
                 <label htmlFor="EndDate">End Date</label>
-                <input type="text" className='rounded-md focus:outline-none focus:ring-2 mb-4 p-1' />
-                <label htmlFor="FirstLocation">First Location</label>
-                <input type="text" className='rounded-md focus:outline-none focus:ring-2 p-1' />
+                <input type="date" className='rounded-md focus:outline-none focus:ring-2 mb-4 p-1' onChange={(e) => setEndDate(e.target.value)}/>
+
             </div>
             <Button text={"Create Trip"} />
         </div>
--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -5,14 +5,14 @@ import logo1 from "../images/logo1.png";
 export const Dashboard = () => {
   return (
     <div className="flex flex-row flex-nowrap h-full">
-      <div className="w-3/4 grid grid-cols-1 grid-rows-5 bg-white">
+      <div className="w-2/3 grid grid-cols-1 grid-rows-5 bg-white">
         <img
           src={logo1}
           alt="Logo"
-          className="w-1/3 row-start-2 row-end-2 justify-self-center"
+          className="w-1/2 row-start-2 row-end-2 justify-self-center"
         />
       </div>
-      <div className="bg-gradient-to-r from-wandernest-blue to-wandernest-pink w-1/4">
+      <div className="bg-gradient-to-r from-wandernest-blue to-wandernest-pink w-1/3">
         <CreateTrip />
       </div>
     </div>
